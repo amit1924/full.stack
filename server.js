c21ebc60e6dafbf64369546864db8f7f700efb5a
amit1924 <amit192400@gmail.com>
2023-09-15 17:29:23 +0530

    2nd update

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(error => console.error("Error connecting to MongoDB:", error));
 
 // Serve static files like CSS
  app.use(express.static("public"));
